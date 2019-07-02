9acbe576907c3775c95499d8ffa2534bcc5d023f
Manash Deb <deb.manash@gmail.com>
2019-07-01 22:46:07 -0400

    corrected setup files

--- initial-setup.bash
+++ initial-setup.bash
@@ -1,14 +1,6 @@
 #!/bin/bash
 echo "install psql"
 yum install -y postgresql96
-
-echo "install git"
-yum -y install git
-
-echo "setup redshift-stored-procedures repository"
-cd /
-git clone https://github.com/manashdeb/redshift-stored-procedures.git
-
 echo "setup data folders"
 mkdir /stockdata
 chmod 777 /stockdata
